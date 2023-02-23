cc9ff0c5a9639dc11c69f2a4c6877d1a853f377a
Jack Kuan <jkuan@yugabyte.com>
2023-02-22 20:49:20 -0500

    Add workarounds and fixes for the local dev environment setup to work in Ubuntu.

--- products_service/docker-entrypoint.sh
+++ products_service/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-until psql -h host.docker.internal -p 5433 -U yugabyte -c 'show server_version' ; do sleep 1 ; done 2>/dev/null
+until psql -h host.docker.internal -p 5433 -U yugabyte -c 'show server_version' ; do
+    echo 'Waiting for yb-tserver (ysql) to be ready ...'; sleep 1
+done  2>/dev/null
+
 [ "$NODE_APP_INSTANCE" = los-angeles ] && npm run seed
-npm start
+exec npm start
